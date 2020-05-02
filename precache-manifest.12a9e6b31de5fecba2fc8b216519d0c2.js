c342d8c00561e5cf8dca2cd5fa17a27fd418f3ea
Shrey Somaiya <z5257343@unsw.edu.au>
2020-05-02 19:16:49 +1000

    Updates

--- precache-manifest.12a9e6b31de5fecba2fc8b216519d0c2.js
+++ precache-manifest.12a9e6b31de5fecba2fc8b216519d0c2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "748be12c1c29bbe815c5f1b7325f4186",
+    "revision": "04d8c632e935b959e6a86c4dc59afa02",
     "url": "/game_of_life/index.html"
   },
   {
-    "revision": "cb52fb3c403f28dbef8d",
+    "revision": "b0e969d74718a1275def",
     "url": "/game_of_life/static/css/main.25bf413c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/game_of_life/static/js/2.755376a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cb52fb3c403f28dbef8d",
-    "url": "/game_of_life/static/js/main.906297bc.chunk.js"
+    "revision": "b0e969d74718a1275def",
+    "url": "/game_of_life/static/js/main.a4381242.chunk.js"
   },
   {
     "revision": "e7c9ab0fed654edfbe0e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game_of_life/precache-manifest.5b31df993f2caa7dcc0382be6275ba08.js"
+  "/game_of_life/precache-manifest.12a9e6b31de5fecba2fc8b216519d0c2.js"
 );
 
 self.addEventListener('message', (event) => {
