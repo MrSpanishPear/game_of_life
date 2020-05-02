ed8ca5e328286594aac6bb9a3c211e5c4a13dea6
Shrey Somaiya <z5257343@unsw.edu.au>
2020-05-02 19:13:17 +1000

    SO MANY BUG FIXES AHHAHAHAHHAHA

--- src/index.js
+++ src/index.js
@@ -4,218 +4,295 @@ import './index.css';
 
 
 class Box extends React.Component{
-	selectBox = () => {
-		this.props.selectBox(this.props.row, this.props.col)
-	}
-	render() {
-		return(
-			<div
-				className={this.props.boxClass}
-				id={this.props.id}
-				onClick={this.selectBox}
-			/>
-		)
-	}
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.col)
+    }
+    render() {
+        return(
+            <div
+                className={this.props.boxClass}
+                id={this.props.id}
+                onClick={this.selectBox}
+            />
+        )
+    }
 }
 
 
 class Grid extends React.Component {
 
-	render() {
-		const width = (this.props.cols * 16);
-		var rowsArr = []
-	 	var boxClass = "";
-		
-		//could be replaced with map function
-		for (let i = 0; i < this.props.rows; i++){
-			for(let j = 0; j < this.props.rows; j++){
-				let boxId = i + "_" + j;
-				boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+    render() {
+        const width = (this.props.cols * 16) + 1;
+        var rowsArr = []
+         var boxClass = "";
+        
+        //could be replaced with map function
+        for (let i = 0; i < this.props.rows; i++){
+            for(let j = 0; j < this.props.cols; j++){
+                let boxId = i + "_" + j;
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
 
-				rowsArr.push(
-					<Box
-						boxClass={boxClass}
-						key={boxId}
-						boxId={boxId}
-						row={i}
-						col={j}
-						selectBox={this.props.selectBox}
+                rowsArr.push(
+                    <Box
+                        boxClass={boxClass}
+                        key={boxId}
+                        boxId={boxId}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
 
-					/>
+                    />
 
-				);
-			}
-		}
+                );
+            }
+        }
 
-		return(
-			<div id="grid" className="grid" style={{width:width}}>
-				{rowsArr}
-			</div>
-		)
-		
-	}
+        return(
+            <div id="grid" className="grid" style={{width:width}}>
+                {rowsArr}
+            </div>
+        )
+        
+    }
 }
 
 
 
 
 class Main extends React.Component {
-	constructor() {
-		super();
-		this.speed = 100;
-		this.rows = 60;
-		this.cols = 100;
-		this.state = {
-			generation: 0,
-			//Fills the grid with FALSE initially
-			gridFull: Array(this.rows).fill().map( () => Array(this.cols).fill(false))
-		}
-	}
+    constructor() {
+        super();
+        this.speed = 300;
+        this.rows = 30;
+        this.cols = 50;
+        this.state = {
+            generation: 0,
+            //Fills the grid with FALSE initially
+            gridFull: Array(this.rows).fill().map( () => Array(this.cols).fill(false))
+        }
+    }
 
-	selectBox = (row,col) =>{
-		//.slice wont work because gridFull is 2D array
-		//https://blog.andrewray.me/how-to-clone-a-nested-array-in-javascript/
-		let grid = JSON.parse(JSON.stringify(this.state.gridFull))
+    selectBox = (row,col) =>{
+        //.slice wont work because gridFull is 2D array
+        //https://blog.andrewray.me/how-to-clone-a-nested-array-in-javascript/
+        let grid = JSON.parse(JSON.stringify(this.state.gridFull))
+        console.log(`calling select box on ${row},${col}`)
+        grid[row][col] = !grid[row][col];
+        this.setState({
+            gridFull: grid
+        })
+    }
 
-		grid[row][col] = !grid[row][col];
-		this.setState({
-			gridFull: grid
-		})
-	}
+    seed = () => {
+        let grid = JSON.parse(JSON.stringify(this.state.gridFull))
+        for (let i = 0; i < this.rows; i++){
+            for(let j = 0; j < this.rows; j++){
+                //random number between 0 and 4, if 1 then: 
+                let random = Math.floor(Math.random() * 4);
+                if(random === 1) {
+                    grid[i][j] = true;
+                }
+            }
+        }
 
-	seed = () => {
-		let grid = JSON.parse(JSON.stringify(this.state.gridFull))
-		for (let i = 0; i < this.rows; i++){
-			for(let j = 0; j < this.rows; j++){
-				//random number between 0 and 4, if 1 then: 
-				let random = Math.floor(Math.random() * 4);
-				if(random === 1) {
-					grid[i][j] = true;
-				}
-			}
-		}
+        this.setState({
+            gridFull: grid
+        });
+    }
 
-		this.setState({
-			gridFull: grid
-		});
-	}
+    playButton = () => {
+        clearInterval(this.intervalId)
+        this.intervalId = setInterval(this.play, this.speed);
+        this.play()
+    }
+    
+    pause = () => {
+        clearInterval(this.intervalId);
+    }
 
-	playButton = () => {
-		clearInterval(this.intervalId)
-		this.intervalId = setInterval(this.play, this.speed);
-	}
-	
-	pause = () => {
-		clearInterval(this.intervalId);
-	}
-	//game rules etc
-	//sourced froms
-	//https://gist.github.com/beaucarnes/1c22fc5e10b15a1f2bf338bf7d09b1b9
-	play = () => {
-		let g = this.state.gridFull;
-		let g2 = JSON.parse(JSON.stringify(this.state.gridFull))
-	  
-		for (let i = 0; i < this.rows; i++) {
-		  for (let j = 0; j < this.cols; j++) {
-			let count = 0;
-			if (i > 0) if (g[i - 1][j]) count++;
-			if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
-			if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
-			if (j < this.cols - 1) if (g[i][j + 1]) count++;
-			if (j > 0) if (g[i][j - 1]) count++;
-			if (i < this.rows - 1) if (g[i + 1][j]) count++;
-			if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
-			if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
-			if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
-			if (!g[i][j] && count === 3) g2[i][j] = true;
-		  }
-		}
-		this.setState({
-		  gridFull: g2,
-		  generation: this.state.generation + 1
-		});
-	  
-	}
+    // sourced with edits from https://medium.com/javascript-in-plain-english/the-game-of-life-using-javascript-fc1aaec8274f
+    getNeighborCount(row, col) {
+        let count = 0;
+        let nrow=Number(row);
+        let ncol=Number(col);
+        let currGen = this.state.gridFull;
+        let rows = this.rows;
+        let cols = this.cols;
 
-	clear = () => {
-		let copy = Array(this.rows).fill().map( () => Array(this.cols).fill(false))
-		this.speed = 200;
-		this.setState({
-			gridFull: copy,
-			generation: 0,
-		});
-		this.pause()
-	}
-	slow = () => {
-		this.speed += 50;
-		this.playButton();
-	}
-	speedUp = () => {
-		this.speed -= 50;
-		this.playButton();
-	}
-	componentDidMount() {
-		this.seed();
-		this.playButton();
-	}
+        // Make sure we are not at the first row
+        if (nrow - 1 >= 0) {
+            // Check top neighbor
+            if (currGen[nrow - 1][ncol] === true) count++;
+        }
+
+        // Make sure we are not in the first cell
+        if (nrow - 1 >= 0 && ncol - 1 >= 0) {
+        //Check upper left neighbor
+            if (currGen[nrow - 1][ncol - 1] === true) count++;
+        }
+
+        // Make sure we are not on the first row last column
+        if (nrow - 1 >= 0 && ncol + 1 < cols) {
+            //Check upper right neighbor
+            if (currGen[nrow - 1][ncol + 1] === true) count++;
+        }
+            
+        // Make sure we are not on the first column
+        if (ncol - 1 >= 0) {
+            //Check left neighbor
+            if (currGen[nrow][ncol - 1] === true) count++;
+        }
+
+        // Make sure we are not on the last column
+        if (ncol + 1 < cols) {
+            
+            //Check right neighbor
+            if (currGen[nrow][ncol + 1] === true) count++;
+        }
+
+        // Make sure we are not on the bottom left corner
+        if (nrow + 1 < rows && ncol - 1 >= 0) {
+            //Check bottom left neighbor
+            if (currGen[nrow + 1][ncol - 1] === true) count++;
+        }
+
+        // Make sure we are not on the bottom right
+        if (nrow + 1 < rows && ncol + 1 < cols) {
+            //Check bottom right neighbor
+            if (ncol==0 && nrow==0) console.log(`bottom right = ${nrow + 1},${ncol +1} = ${currGen[nrow + 1][ncol + 1]}`)
+
+            if (currGen[nrow + 1][ncol + 1] === true) count++;
+        }
+        
+        
+        // Make sure we are not on the last row
+        if (nrow + 1 < rows) {
+            //Check bottom neighbor
+            if (currGen[nrow + 1][ncol] === true) count++;
+        }
+        
+        return count;
+    }
+
+    //game rules etc
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = JSON.parse(JSON.stringify(this.state.gridFull))
+      
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let neighbors =this.getNeighborCount(i, j);
+                if ( i< 1 && j < 1) console.log(i,j, neighbors);
+                // Check the rules
+                // If Alive
+                if (g[i][j] === true) {
+                  
+                    if (neighbors < 2) {
+                        g2[i][j] = false;
+                    } else if (neighbors === 2 || neighbors === 3) {
+                        g2[i][j] = true;
+                    } else if (neighbors > 3) {
+                        g2[i][j] = false;
+                    }
+                } else if (g[i][j] === false) {
+                    // If Dead or Empty
+                
+                    if (neighbors === 3) {
+                        // Propogate the species
+                        g2[i][j] = true;//Birth?
+                    }
+                }
+            }
+        }
+
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+        });
+
+        this.forceUpdate(); 
+      
+    }
+
+    clear = () => {
+        let copy = Array(this.rows).fill().map( () => Array(this.cols).fill(false))
+        this.speed = 300;
+        this.setState({
+            gridFull: copy,
+            generation: 0,
+        });
+        this.pause()
+    }
+    slow = () => {
+        this.speed += 50;
+        this.forceUpdate(); 
+    }
+    speedUp = () => {
+        this.speed -= 50;
+        this.forceUpdate(); 
+    }
+    componentDidMount() {
+        // this.seed();
+    }
+
     render() {
         return (
             <div>
                 <h1>
-					Conways Game of Life!
-				</h1>
-				<Grid
-					gridFull={this.state.gridFull}
-					rows={this.rows}
-					cols={this.cols}
-					selectBox={this.selectBox}
-				/>
-				<h2>
-					Generation: {this.state.generation}
-				</h2>
-				<h2>
-					Speed: Update every {this.speed}ms
-				</h2>
-				<div className="buttonContainer">
-					<button 
-						onClick={this.playButton}
-						className={"button"}
+                    Conways Game of Life!
+                </h1>
+                <Grid
+                    gridFull={this.state.gridFull}
+                    rows={this.rows}
+                    cols={this.cols}
+                    selectBox={this.selectBox}
+                />
+                <h2>
+                    Generation: {this.state.generation}
+                </h2>
+                <h2>
+                    Speed: Update every {this.speed}ms
+                </h2>
+                <div className="buttonContainer">
+                    <button 
+                        onClick={this.playButton}
+                        className={"button"}
 
-					>
-						Play
-					</button>
-					<button
-						onClick={this.pause}
-						className={"button"}
-					>
-						Pause
-					</button>
-					<button
-						onClick={this.clear}
-						className={"button"}
-					>
-						Clear
-					</button>
-					<button
-						onClick={this.seed}
-						className={"button"}
-					>
-						Seed
-					</button>
-					<button
-						onClick={this.slow}
-						className={"button"}
-					>
-						Slow
-					</button>
-					<button
-						onClick={this.speedUp}
-						className={"button"}
-					>
-						Speed
-					</button>
-				</div>
-				
+                    >
+                        Play
+                    </button>
+                    <button
+                        onClick={this.pause}
+                        className={"button"}
+                    >
+                        Pause
+                    </button>
+                    <button
+                        onClick={this.clear}
+                        className={"button"}
+                    >
+                        Clear
+                    </button>
+                    <button
+                        onClick={this.seed}
+                        className={"button"}
+                    >
+                        Seed
+                    </button>
+                    <button
+                        onClick={this.slow}
+                        className={"button"}
+                    >
+                        Slow
+                    </button>
+                    <button
+                        onClick={this.speedUp}
+                        className={"button"}
+                    >
+                        Speed
+                    </button>
+                </div>
+                
             </div>
         )
     } 
--- src/playlistTime.js
+++
@@ -1,8 +0,0 @@
-let url = 'https://www.youtube.com/watch?v=Oi4v5uxTY5o&list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3';
-
-fetch(url)
-.then((resp)=>{
-    console.log(resp);
-
-})
-
