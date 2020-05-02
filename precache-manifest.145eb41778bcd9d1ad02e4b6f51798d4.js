9e619ad79bd45f275281c56c15ff03b9cecca50d
Shrey Somaiya <z5257343@unsw.edu.au>
2020-05-02 17:45:41 +1000

    Updates

---
+++ precache-manifest.145eb41778bcd9d1ad02e4b6f51798d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c8728bf3a2568eb4422e020296eb0d",
+    "url": "/game_of_life/index.html"
+  },
+  {
+    "revision": "a223247ade4c0e9df5e0",
+    "url": "/game_of_life/static/css/main.bd9be51a.chunk.css"
+  },
+  {
+    "revision": "f489a9fd59dc10b70f4a",
+    "url": "/game_of_life/static/js/2.755376a9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/game_of_life/static/js/2.755376a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a223247ade4c0e9df5e0",
+    "url": "/game_of_life/static/js/main.77908d86.chunk.js"
+  },
+  {
+    "revision": "e7c9ab0fed654edfbe0e",
+    "url": "/game_of_life/static/js/runtime-main.3916ebef.js"
+  }
+]);
